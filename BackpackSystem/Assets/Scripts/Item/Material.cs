ee6545c29394ad63420a14a897560f72a198149b
KIPKIPS <2432719656@qq.com>
2021-04-05 20:08:05 +0800

    目录重构

--- BackpackSystem/Assets/Scripts/Item/Material.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//材料类
-public class Material : Item {
-
-}
---
+++ InventorySystem/Assets/Scripts/Item/Consumable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//消耗品类
+public class Consumable : Item {
+    // Start is called before the first frame update
+    public int HP { get; set; }
+    public int MP { get; set; }
+    //继承父类的构造方法
+    public Consumable(int _id, string _name, ItemType _itemType, Quality _qualityType, string _desc, int _capacity, int _buyPrice, int _sellPrice, string _sprite,
+        int _hp, int _mp)
+        : base(_id, _name, _itemType, _qualityType, _desc, _capacity, _buyPrice, _sellPrice, _sprite) {
+        HP = _hp;
+        MP = _mp;
+    }
+}
