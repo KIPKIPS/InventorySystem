d582bd4484101133b48a8fcc3e7f532b13092188
KIPKIPS <2432719656@qq.com>
2021-04-05 14:34:29 +0800

    框架图和装备类

---
+++ BackpackSystem/Assets/Scripts/Item/Equipment.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equipment : Item {
+    public Equipment(int _id, string _name, ItemType _itemType, Quality _qualityType, string _desc, int _capacity, int _buyPrice, int _sellPrice)
+        : base(_id, _name, _itemType, _qualityType, _desc, _capacity, _buyPrice, _sellPrice) {
+
+    }
+
+}
