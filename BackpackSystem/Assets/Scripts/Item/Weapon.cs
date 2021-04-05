3fd5b4c19f6396b22bec6988f00f051b1618758e
KIPKIPS <2432719656@qq.com>
2021-04-05 17:31:05 +0800

    武器类

---
+++ BackpackSystem/Assets/Scripts/Item/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Item {
+    public int Damage { get; set; }
+    public WeaponType Type { get; set; }
+    //继承父类的构造方法
+    public Weapon(int _id, string _name, ItemType _itemType, Quality _qualityType, string _desc, int _capacity, int _buyPrice, int _sellPrice, int _damage, WeaponType _type)
+        : base(_id, _name, _itemType, _qualityType, _desc, _capacity, _buyPrice, _sellPrice) {
+        Type = _type;
+        Damage = _damage;
+    }
+    public enum WeaponType {
+        MainHand,
+        OffHand,
+    }
+}
